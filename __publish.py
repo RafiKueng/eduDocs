e14fd40281a9bf60d921e87f5c72a7301af6c895
RafiKueng <rafi.kueng@gmx.ch>
2013-11-28 18:04:59 +0100

    build script

---
+++ __publish.py
@@ -0,0 +1,42 @@
+import os
+import shutil
+import os.path as p
+import errno
+
+srcfiles = [
+  '_lib',
+  '_pages',
+  '_templates',
+  'css',
+  'img',
+  'js',
+  '404.php',
+  #'config.php',
+  'favicon.ico',
+  'index.php',
+]
+
+#host specific files
+hostfiles = [
+  ('config','.php'),
+]
+
+host = 'physik'
+srcpath = p.abspath('.')
+dest = p.abspath('./bla/')
+
+for f in srcfiles:
+  print 'copying:', (p.join(srcpath,f),p.join(dest,f)),
+  if p.isdir(p.join(srcpath,f)):
+    shutil.copytree(p.join(srcpath,f),p.join(dest,f))
+  else:
+    shutil.copy(p.join(srcpath,f),p.join(dest,f))
+    print ' (f)',
+  print '   [ ok ]'
+    
+for (n, ext) in hostfiles:
+  try:
+    print 'copying:', (p.join(srcpath,n+'.'+host+ext),p.join(dest,n+ext))
+    shutil.copy(p.join(srcpath,n+'.'+host+ext),p.join(dest,n+ext))
+  except OSError as e:
+    raise e
